68943e2bb3729c051bed1174e59e7ab44b087764
ShamiLok <shmi1din@gmail.com>
2025-06-02 22:37:18 +0500

    mapping added/change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,4 @@
+-- vim.o.langmap = "й,q,ц,w,у,e,к,r,е,t,н,y,г,u,ш,i,щ,o,з,p,х,[,ъ,],ф,a,ы,s,в,d,а,f,п,g,р,h,р,j,о,k,л,l,д,z,ж,x,э,c,я,v,ч,b,с,n,ш,m,м,<,я,>,ё"
 -- vim.keymap.set("n", "<leader>n", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
@@ -31,11 +32,15 @@ vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = "Telescope find fi
 vim.keymap.set('n', '<leader>r', builtin.live_grep,  { desc = "Telescope live grep" })
 
 -- switch buffer
-vim.api.nvim_set_keymap('n', '<leader>bn', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>bp', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>h', ':bprevious<CR>', { noremap = true, silent = true })
 
 --comments 
 vim.keymap.set("n", "<leader>c", "<Plug>NERDCommenterToggle", {})
 vim.keymap.set("v", "<leader>c", "<Plug>NERDCommenterToggle", {})
 
+-- get changes
 vim.api.nvim_set_keymap('n', '<leader>g', ':Gitsigns preview_hunk<CR>', { noremap = true, silent = true })
+
+-- vsplit
+vim.keymap.set("n", "<leader>v", ":vsplit<Space>", { noremap = true })
